c7c4f23525a3c0a82eb5db962162ef81c2203c9a
valerie schraauwers <valerieschraauwers@gmail.com>
2020-07-03 16:38:29 +0200

    Permit parameter :photo in articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -53,6 +53,6 @@ class ArticlesController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def article_params
-      params.require(:article).permit(:title, :body)
+      params.require(:article).permit(:title, :body, :photo)
     end
 end
