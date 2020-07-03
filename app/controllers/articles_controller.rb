9f912866eb0efa7c31e06670556dfb226a236767
valerie schraauwers <valerieschraauwers@gmail.com>
2020-07-03 16:42:40 +0200

    Change article form and permitted paramaters in the controller to accept multiple photos

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -53,6 +53,6 @@ class ArticlesController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def article_params
-      params.require(:article).permit(:title, :body, :photo)
+      params.require(:article).permit(:title, :body, photos: [])
     end
 end
