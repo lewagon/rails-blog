ed647e31c03b0d372b9aff339de4347d725960ba
valerie schraauwers <valerieschraauwers@gmail.com>
2020-07-03 17:03:46 +0200

    Create migration for removing body column from articles, will now be handled through Action Text

---
+++ db/migrate/20200703150239_remove_body_from_articles.rb
@@ -0,0 +1,5 @@
+class RemoveBodyFromArticles < ActiveRecord::Migration[6.0]
+  def change
+    remove_column :articles, :body
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_03_145808) do
+ActiveRecord::Schema.define(version: 2020_07_03_150239) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,7 +48,6 @@ ActiveRecord::Schema.define(version: 2020_07_03_145808) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
-    t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
