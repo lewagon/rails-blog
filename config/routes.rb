afc244eaf9e4c5b69773254d93627adfb870e304
valerie schraauwers <valerieschraauwers@gmail.com>
2020-07-03 16:18:29 +0200

    Add Article scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  root to: 'articles#index'
+  resources :articles, except: :index
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
