59529e44e13a7f5a0f5345677b71b96e7fe9f148
Alexander Dubovoy <ajdubovoy@gmail.com>
2024-07-25 17:15:37 +0200

    Upgrade this repo to Rails 7

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,6 @@
 
 require "open-uri"
 
-file = URI.open('https://giantbomb1.cbsistatic.com/uploads/original/9/99864/2419866-nes_console_set.png')
+file = URI.open('https://upload.wikimedia.org/wikipedia/commons/8/82/NES-Console-Set.jpg')
 article = Article.new(title: 'NES', body: "A great console")
-article.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+article.photos.attach(io: file, filename: 'nes.png', content_type: 'image/png')
